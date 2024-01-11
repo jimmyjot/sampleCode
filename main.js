8a8a1591b364d34ffd3c4d40efd83d46998b6921
James Tarsi <jimmyjot@gmail.com>
2024-01-11 13:14:34 -0600

    Remove commented code (from examples)

--- main.js
+++ main.js
@@ -20,28 +20,3 @@ function clearYesNo(dateResult) {
   document.getElementById("clear").value = buttonClear;
   console.log("clearYesNo - end");
 }
-
-/* const buttonName = document.querySelector(".eventName");
-
-function changeColor(color) {
-  buttonName.style.color = color;
-}
-
-const btn = document.querySelector("button");
-
-btn.addEventListener("click", function () {
-  console.log("addEventListener - begin");
-  buttonName.style.color = "blue";
-  console.log("addEventListener - end");
-});
-
-document.getElementById("dateEntry").addEventListener("click", function (d) {
-  console.log("confirm(d) - event listener - begin");
-  if (d instanceof Date) {
-    isDate = "yes";
-  } else {
-    isDate = "no";
-  }
-  console.log("confirm(d) - event listener - end");
-});
- */
