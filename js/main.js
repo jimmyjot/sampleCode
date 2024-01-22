4939d5c337ba5f59a4f7a6e26c61cc5108c00494
James Tarsi <jimmyjot@gmail.com>
2024-01-22 08:44:19 -0600

    Added comments

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let buttonClear = document.getElementById("id_clearField");
+/* let buttonClear = document.getElementById("id_clearField");
 buttonClear.addEventListener("click", clearYesNo());
 
 let dateResult = document.getElementById("isDate");
@@ -11,12 +11,21 @@ function clearYesNo(dateResult) {
   console.log("clearYesNo - end");
 }
 
+ */
+
+// Functions to write:
+// checkDate()
+// clearField()
+// setDate()
+
 function checkDate(d) {
   console.log("checkDate(d) - begin");
-  if (!isNaN(new Date(d))) {
+  if (!isNaN(d)) {
     inputDate.style.color = "green";
+    document.getElementById("id_isDate").innerHTML = "Yes";
   } else {
     inputDate.style.color = "red";
+    document.getElementById("id_isDate").innerHTML = "No";
   }
   console.log("checkDate(d) - end");
 }
